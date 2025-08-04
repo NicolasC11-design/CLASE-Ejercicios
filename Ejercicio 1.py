0bb4042d5a641a32e882dd9ed41562e80e5f1bec
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2025-08-04 09:50:54 -0500

    ejercicio1

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -3,11 +3,13 @@ Dic2 = {"Nombre": "Sebastian", "Apellido":  "Murcia", "Notas": [1.0, 2.0, 3.0, 4
 Dic3 = {"Nombre": "Juan", "Apellido":  "Daza", "Notas": [3.5, 4.0, 5.0, 5.0, 5.0]}
 Dic4 = {"Nombre": "David", "Apellido":  "Anzola", "Notas": [5.0, 4.0, 4.0, 4.5, 2.5]}
 Dic5 = {"Nombre": "Felipe", "Apellido":  "Mendieta", "Notas": [3.0, 2.0, 4.0, 4.5, 3.0]}
+
 Prom1 = (Dic1["Notas"][0] + Dic1["Notas"][1] + Dic1["Notas"][2] + Dic1["Notas"][3] + Dic1["Notas"][4]) / 5
 Prom2 = (Dic2["Notas"][0] + Dic2["Notas"][1] + Dic2["Notas"][2] + Dic2["Notas"][3] + Dic2["Notas"][4]) / 5
 Prom3 = (Dic3["Notas"][0] + Dic3["Notas"][1] + Dic3["Notas"][2] + Dic3["Notas"][3] + Dic3["Notas"][4]) / 5
 Prom4 = (Dic4["Notas"][0] + Dic4["Notas"][1] + Dic4["Notas"][2] + Dic4["Notas"][3] + Dic4["Notas"][4]) / 5
 Prom5 = (Dic5["Notas"][0] + Dic5["Notas"][1] + Dic5["Notas"][2] + Dic5["Notas"][3] + Dic5["Notas"][4]) / 5
+
 print("Promedio de Notas de Dic1 es:", Prom1) 
 print("Promedio de Notas de Dic2 es:", Prom2)
 print("Promedio de Notas de Dic3 es:", Prom3)  
