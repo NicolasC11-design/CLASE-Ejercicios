b5d10946605ccfe34a3d926ca33d7af58f392e0a
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2025-08-04 10:42:01 -0500

    ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,13 @@
+print("************************************************************")
+print("*     La suma de las operaciones es:                       *")
+a=40
+b=40
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+print ("*   Suma: ",suma,"                                             *")
+print ("*   Resta: ",resta,"                                             *")
+print ("*   Multiplicación: ",multiplicacion,"                                 *")
+print("*    Este Programa fue Realizado por Nicolas Cristiano     *")  
+
+print("************************************************************")
